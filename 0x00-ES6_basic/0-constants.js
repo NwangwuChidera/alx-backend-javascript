06b7de7144c3b7adc54c030f95ac3e63ca689108
NwangwuChidera <nwangwupamela@gmail.com>
2023-06-27 01:49:32 -0700

    task 0

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,19 @@
+
+// Modified taskFirst function using const
+export function taskFirst() {
+  const firstName = 'John';
+  const lastName = 'Doe';
+
+  console.log(`First Name: ${firstName}`);
+  console.log(`Last Name: ${lastName}`);
+}
+
+// Modified taskNext function using let
+export function taskNext() {
+  let age = 25;
+  let city = 'New York';
+
+  console.log(`Age: ${age}`);
+  console.log(`City: ${city}`);
+}
+
