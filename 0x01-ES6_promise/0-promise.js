008ce798e38a02932d12e63051960bad09c5634c
NwangwuChidera <nwangwupamela@gmail.com>
2023-06-27 14:13:31 -0700

    task

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,18 @@
+// Example prototype function
+function MyAPI() {
+  MyAPI.prototype.getResponseFromAPI = function() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const response = 'API response data';
+        resolve(response);
+      }, 2000);
+    });
+  };
+}
+
+// Usage example
+const myAPI = new MyAPI();
+myAPI.getResponseFromAPI().then((response) => {
+  console.log('API Response:', response);
+});
+
