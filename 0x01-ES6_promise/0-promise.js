1b2443f0e7c47b2c6e823174c62362d82d08161d
NwangwuChidera <nwangwupamela@gmail.com>
2023-06-27 14:34:16 -0700

    task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,21 +1,7 @@
-// Example prototype function
-function MyAPI() {}
-
-// Prototype method that returns a Promise
-MyAPI.prototype.getResponseFromAPI = function() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      const response = 'API response data';
-      resolve(response);
-    }, 2000);
+export default function getResponseFromAPI() {
+  const promise = new Promise((resolve, reject) => {
+    resolve();
+    reject();
   });
-};
-
-// Usage example
-const myAPI = new MyAPI();
-myAPI.getResponseFromAPI().then((response) => {
-  console.log('API Response:', response);
-}).catch((error) => {
-  console.error('Error:', error);
-});
-
+  return promise;
+}
