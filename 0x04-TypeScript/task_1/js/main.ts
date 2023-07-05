48b151b880f6594ef40c20b153eab79bc5974947
NwangwuChidera <nwangwupamela@gmail.com>
2023-07-05 13:41:42 -0700

    tasks

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -17,4 +17,47 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Example usage:
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  const initial = firstName.charAt(0).toUpperCase();
+  return initial + '. ' + lastName;
+};
+
+// Example usage:
+const result = printTeacher("John", "Doe");
+console.log(result); // Output: J. Doe
+
+interface StudentClassInterface {
+  new(firstName: string, lastName: string): StudentClass;
+}
+
+class StudentClass {
+  constructor(public firstName: jon, public lastName: snow) {}
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
 
